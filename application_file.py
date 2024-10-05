5ad94e866842482818bf6277b87f72d6045048e6
Vivek Arya <vivekveersainarya@gmail.com>
2024-10-05 23:28:32 +0530

    Minor Changes!

--- application_file.py
+++ application_file.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import threading
 import time
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 from lxml import html as p_html
 import dash
@@ -32,7 +32,6 @@ class Data:
     def run_check(self):
         while self.running:
             self.update()
-            #print("Sleeping...", self.check_interval)  # Consider toggling this
             time.sleep(self.check_interval)
 
     def update(self):
@@ -43,7 +42,6 @@ class Data:
     def get_data(self):
         if self.last_modified is not None:
             self.simulate_data()
-            #print("Sum of Margins:", self.data.Margin.sum())  # Consider toggling this
             return
         else:
             df = self.fetch(self.location)
@@ -55,10 +53,8 @@ class Data:
 
     def simulate_data(self):
         if not self.data.empty:
-            # Update the selected row for Margin and Leading Party
             self.data.at[random.randint(0, len(self.data) - 1), "Margin"] = random.randint(2000, 120000)
             self.data.at[random.randint(0, len(self.data) - 1), "Leading Party"] = random.choice(["BJP", "INC", "INLD", "AAP", "Others"])
-
             self.data = self.data.sort_values(by="Margin", ascending=False)
             self.data["Seat"] = "X" + self.data["Const. No."].astype(str)
             self.last_modified = time.time()  # Update last modified time
@@ -68,21 +64,7 @@ class Data:
             page = requests.get(
                 "https://results.eci.gov.in/PcResultGenJune2024/%s.htm" % location,
                 headers={
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                    "Accept-Encoding": "gzip, deflate, br, zstd",
-                    "Accept-Language": "en-US,en;q=0.5",
-                    "Connection": "keep-alive",
-                    "DNT": "1",
-                    "Priority": "u=1",
-                    "Referer": "https://results.eci.gov.in/PcResultGenJune2024/%s.htm" % location,
-                    "Sec-Fetch-Dest": "document",
-                    "Sec-Fetch-Mode": "navigate",
-                    "Sec-Fetch-Site": "same-origin",
-                    "Sec-Fetch-User": "?1",
-                    "Sec-GPC": "1",
-                    "TE": "trailers",
-                    "Upgrade-Insecure-Requests": "1",
-                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0"
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
                 }
             )
             page.raise_for_status()  # Raise an error for bad responses
@@ -129,80 +111,77 @@ app.layout = html.Div(
     children=[
         html.H1(
             children=[
-                html.H1(
-                        children=[
-                                    html.A(
-                                        "Haryana Election Results: 2024",
-                                        href="https://www.youtube.com/@HaryanaAurHaryanvi?sub_confirmation=1",
-                                        target="_blank",  # Opens in a new tab
-                                        style={'textDecoration': 'none', 'color': 'inherit'}  # Remove underline and inherit color
-                                    )
-                                ],
-                        className="text-center",
-                        style={'marginBottom': '0.1rem', 'fontSize': '3rem'}  # Adjusted margin for spacing
-                    ),
-
                 html.A(
-                        children=html.P(
-                            "Click and Subscribe us on Youtube to support this development",
-                            className="text-center",
-                            style={'marginBottom': '2rem', 'fontSize': '1.2rem', 'color': '#555', 'textDecoration': 'none'}  # Style as needed
-                        ),
-                        href="https://www.youtube.com/@HaryanaAurHaryanvi?sub_confirmation=1",  # Link to subscribe
-                        target="_blank",  # Opens in a new tab
-                    )
-                            ],
+                    "Haryana Election Results: 2024",
+                    href="https://www.youtube.com/@HaryanaAurHaryanvi?sub_confirmation=1",
+                    target="_blank",
+                    style={'textDecoration': 'none', 'color': 'inherit'}
+                )
+            ],
             className="text-center",
-            style={'marginBottom': '2rem', 'fontSize': '3rem'}  # Increased font size
+            style={'marginBottom': '0.1rem', 'fontSize': '3rem'}
         ),
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        dcc.Dropdown(data.data["Constituency"].sort_values(),
-                                          multi=True,
-                                          id='seat-selector',
-                                          placeholder="Filter for Assembly",
-                                          style={'width': 'auto', 'minWidth': '250px', 'margin': '0.1em'}
-                                          ),
-                        html.Div(
-                            id="loading",
-                            children=[dcc.Graph(id='election-graph', config={'displayModeBar': False})],
-                            #type="default"
-                        ),
-                    ],
-                    width=6.00
-                ),
-                dbc.Col(
-                    [
-                        html.Div(id='timestamp', style={'textAlign': 'center', 'marginBottom': '10px'}),
+                        html.Div(id='timestamp', style={'textAlign': 'center', 'marginBottom': '1px'}),
                         html.Div(
                             children=[dcc.Graph(id='party-pie-chart', config={'displayModeBar': False})],
-                            #type="default"
+                            style={'width': '100%'}
                         ),
                         html.Div(
                             children=[dcc.Graph(id='least-margin-bar-chart', config={'displayModeBar': False})],
-                            #type="default"
+                            style={'width': '100%'}
                         ),
                     ],
-                    width=6
+                    width=8,
+                    xs=12,
+                    sm=12,
+                    md=6,  # Half width on medium devices
                 ),
-            ]
+                dbc.Col(
+                    [
+                        dcc.Dropdown(
+                            data.data["Constituency"].sort_values(),
+                            multi=True,
+                            id='seat-selector',
+                            placeholder="Filter for Assembly",
+                            style={'width': '100%', 'margin': '0.1em'}
+                        ),
+                        html.Div(
+                            id="loading",
+                            children=[dcc.Graph(id='election-graph', config={
+                                'displayModeBar': False,
+                                'editable': False,
+                                'scrollZoom': False,
+                                'doubleClick': 'reset',  # Change this as needed
+                                'modeBarButtonsToRemove': ['lasso2d', 'select2d'], })],
+                            style={'width': '100%'}
+                        ),
+                    ],
+                    width=8,
+                    xs=12,
+                    sm=12,
+                    md=6,  # Half width on medium devices
+                ),
+            ],
+            style={'margin': '0'}
         ),
         html.Footer(
-                    style={'textAlign': 'center', 'padding': '1rem', 'backgroundColor': '#f8f9fa'},
-                    children=[
-                        html.A(
-                            children=html.P(
-                                "Haryana Elections 2024 | © HaryanaAurHaryanvi",
-                                style={'margin': '0'}
-                            ),
-                            href="https://www.youtube.com/@HaryanaAurHaryanvi?sub_confirmation=1",  # Link to subscribe
-                            target="_blank",  # Opens in a new tab
-                            style={'textDecoration': 'none', 'color': '#555'}  # Style as needed
-                        )
-                    ]
-                ),
+            style={'textAlign': 'center', 'padding': '1rem', 'backgroundColor': '#f8f9fa'},
+            children=[
+                html.A(
+                    children=html.P(
+                        "Haryana Elections 2024 | © HaryanaAurHaryanvi",
+                        style={'margin': '0'}
+                    ),
+                    href="https://www.youtube.com/@HaryanaAurHaryanvi?sub_confirmation=1",
+                    target="_blank",
+                    style={'textDecoration': 'none', 'color': '#555'}
+                )
+            ]
+        ),
         dcc.Interval(id='interval-component', interval=5 * 1000, n_intervals=0),
         dcc.Store(id='intermediate-value', data={"total": 0})
     ]
@@ -225,21 +204,26 @@ def create_least_margin_bar_chart(df):
         'layout': {
             'title': 'Top 5 Seats with Least Margin',
             'height': 400,
-            'width': 600,
-            "margin": {'l': 10},
+            'width': 400,
+            "margin": {'l': 0},
             'titlefont': {'size': 18},
+            'font': {
+                'size': 12  # Set the size for the text labels
+            },
+            'dragmode': False ,
             'xaxis': {
                 'title': 'Margin'
             },
             'yaxis': {
                 'title': '',
                 'showticklabels': False,
-            }
+            },
+            
         }
     }
     return bar_chart
 
-def stack_data(df, existing_texts=None, height=850):
+def stack_data(df, existing_texts=None):
     stack = []
     existing_colors = set(color_map.values())
 
@@ -260,7 +244,11 @@ def stack_data(df, existing_texts=None, height=850):
             'name': party,
             'marker': {'color': color_map[party]},
             "orientation": 'h',
-            "config": {'displayModeBar': False}
+            "config": {
+                'displayModeBar': False,  # Disable the display mode bar
+                'scrollZoom': False,       # Disable zooming with scroll
+                'displaylogo': False       # Hide the plotly logo
+            }
         }
 
         stack.append(item)
@@ -271,12 +259,16 @@ def stack_data(df, existing_texts=None, height=850):
             "title": "Election Margin by Constituency",
             "transition": {'duration': 1500},
             "height": max(len(df) * 80, 400),
-            "width": 850,
-            "margin": {'l': 0, 'r': 150},#, 't': 120, 'b': 40},
+            "width": 350,
+            "margin": {'l': 0.00, 'r': 0},
+            'font': {
+                'size': 14  # Set the size for the text labels
+            },
+            'dragmode': False ,
             "legend": {
                 "orientation": "h",
                 "yanchor": "bottom",
-                "y": 1.02,
+                "y": 1.0,
                 "xanchor": "center",
                 "x": 0.5
             },
@@ -289,6 +281,7 @@ def stack_data(df, existing_texts=None, height=850):
 
     return data, existing_texts
 
+
 def create_pie_chart(df):
     pie_data = df['Leading Party'].value_counts()
     pie_chart = {
@@ -300,17 +293,21 @@ def create_pie_chart(df):
             'marker': {'colors': [color_map.get(party, "#CCCCCC") for party in pie_data.index]},
         }],
         'layout': {
-            'title': 'Distribution of Seats by Party',
-            'height': 600,
-            'width': 600,
+            'title': '',
+            'height': 400,
+            'width': 400,  # Set width to 100% for responsiveness
             "margin": {'l': 0},
             'titlefont': {'size': 18},
+            'showlegend': False,
+            'font': {
+                'size': 20  # Set the size for the text labels
+            },
             "legend": {
-                "orientation": "h",
-                "yanchor": "bottom",
-                "y": -0.1,
-                "xanchor": "center",
-                "x": 0.5
+                'orientation': 'h',
+                'yanchor': 'bottom',
+                'y': 1.05,
+                'xanchor': 'center',
+                'x': 0.5
             }
         }
     }
@@ -334,12 +331,12 @@ def update_graphs(selected_seats, store, n):
     else:
         filtered_df = df
 
-    #print(store["total"], filtered_df.Margin.sum())
+    utc_now = datetime.utcnow()  
+    india_time = utc_now + timedelta(hours=5, minutes=30)  # Adjust to UTC +5:30
+    timestamp = india_time.strftime('%Y-%m-%d %H:%M:%S')
 
     if filtered_df.Margin.sum() == store["total"]:
-        return dash.no_update, dash.no_update, dash.no_update, dash.no_update, {"total": store["total"]}
-
-    timestamp = f"Last Updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        return dash.no_update, dash.no_update, dash.no_update, timestamp, {"total": store["total"]}
 
     election_fig, existing_texts = stack_data(filtered_df)
     party_pie_fig = create_pie_chart(df)
@@ -348,4 +345,4 @@ def update_graphs(selected_seats, store, n):
     return election_fig, party_pie_fig, least_margin_fig, timestamp, {"total": filtered_df.Margin.sum()}
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host = "0.0.0.0", port = 18081)
+    app.run_server(debug=True, host="0.0.0.0", port=18081)
